c1883442357e1823ca9c878b95c59cf4926d8bf0
Harindu Liyanage <harindu.sul@gmail.com>
2020-04-18 18:26:43 +0530

    create a auth module and create sign in sign up components

--- front-end/gladen-web/src/app/auth/auth.module.ts
+++ front-end/gladen-web/src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SignUpComponent, SignInComponent],
   imports: [
     CommonModule
   ]
