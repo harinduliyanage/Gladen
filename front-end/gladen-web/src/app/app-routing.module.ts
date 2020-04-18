494875bdab5341500576cf1e8baeb2a305ef95c7
Harindu Liyanage <harindu.sul@gmail.com>
2020-04-18 18:27:44 +0530

    added basic routing to application

--- front-end/gladen-web/src/app/app-routing.module.ts
+++ front-end/gladen-web/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SignUpComponent} from './auth/sign-up/sign-up.component';
+import {SignInComponent} from './auth/sign-in/sign-in.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'registration', component: SignUpComponent},
+  { path: 'sign-in', component: SignInComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
