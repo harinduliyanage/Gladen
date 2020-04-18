698901b7f48eb3a0a8180488de336ceb4003612a
Harindu Liyanage <harindu.sul@gmail.com>
2020-04-18 19:29:12 +0530

    created common component module for application

---
+++ front-end/gladen-web/src/app/common/app-common.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+
+@NgModule({
+  declarations: [FooterComponent, NavigationComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppCommonModule { }
