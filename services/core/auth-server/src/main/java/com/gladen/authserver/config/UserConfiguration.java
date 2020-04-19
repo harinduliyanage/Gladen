c94f4517c55f244508c84dfde42e6df474a4cfd2
Harindu Liyanage <harindu.sul@gmail.com>
2020-04-19 19:21:03 +0530

    create a basic auth server for testing

---
+++ services/core/auth-server/src/main/java/com/gladen/authserver/config/UserConfiguration.java
@@ -0,0 +1,23 @@
+package com.gladen.authserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("harindu")
+                .password(passwordEncoder.encode("a12345"))
+                .roles("USER")
+                .authorities("CAN_READ","CAN_WRITE");
+    }
+
+}
